d843e7b498cb6efd07506a55100483de1dfd2f0c
MahidulHasan0011 <mahidulhasaneashat169@gmail.com>
2025-01-08 15:54:02 +0600

    convert currency helpher into class based

--- app/helpers/currency.js
+++ app/helpers/currency.js
@@ -1,13 +1,15 @@
-import { helper } from '@ember/component/helper';
+import Helper from '@ember/component/helper';
 
-export default helper(function currency(params, hash) {
- const [number] = params;
- const {sign} = hash;
- const dollers = Math.floor(number);
- let cents = Math.floor(number * 100 % 100);
+export default class currency extends Helper {
+ compute(params, hash){
+  const [number] = params;
+  const {sign} = hash;
+  const dollers = Math.floor(number);
+  let cents = Math.floor(number * 100 % 100);
 
- if(cents.toString().length === 1){
-  cents = '0' + cents;
+  if(cents.toString().length === 1){
+    cents = '0' + cents;
+   }
+   return `${sign}${dollers}.${cents}`;
  }
-  return `${sign}${dollers}.${cents}`;
-});
+}
